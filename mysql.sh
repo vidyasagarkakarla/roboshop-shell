2c9976f28616f398a4bd674fb82bb310eb03ae1e
vidyasagarkakarla <vidyasagarkakarla16@gmail.com>
2024-01-24 16:47:27 +0530

    roboshop shell scripts

--- mysql.sh
+++ mysql.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
 
 ID=$(id -u)
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
 MONGDB_HOST=mongodb.vidyasagar.online
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
-VALIDATE() {
+VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2.....  $R FAILED $N"
+        echo -e "$2 ... $R FAILED $N"
         exit 1
     else
-        echo -e "$2.... $G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 if [ $ID -ne 0 ]
-then 
+then
     echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1 # you can give other than 0
 else
@@ -38,20 +36,20 @@ VALIDATE $? "Disable current MySQL version"
 
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>> $LOGFILE
 
-VALIDATE $? "Cpoied MySQL repo"
+VALIDATE $? "Copied MySQl repo"
 
 dnf install mysql-community-server -y &>> $LOGFILE
 
 VALIDATE $? "Installing MySQL Server"
 
-systemctl enable mysqld &>> $LOGFILE
+systemctl enable mysqld &>> $LOGFILE 
 
 VALIDATE $? "Enabling MySQL Server"
 
 systemctl start mysqld &>> $LOGFILE
 
-VALIDATE $? "Starting MySQL Server"
+VALIDATE $? "Starting  MySQL Server" 
 
 mysql_secure_installation --set-root-pass RoboShop@1 &>> $LOGFILE
 
-VALIDATE $? "Setting MySQL root password"
+VALIDATE $? "Setting  MySQL root password"
