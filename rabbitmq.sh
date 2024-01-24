f964bb6931edc837874cea6a186240c7e39a0c92
vidyasagarkakarla <vidyasagarkakarla16@gmail.com>
2024-01-24 16:58:50 +0530

    roboshop shell scripts

---
+++ rabbitmq.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGDB_HOST=mongodb.vidyasagar.online
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> $LOGFILE
+
+VALIDATE $? "Downloading erlang script"
+
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> $LOGFILE
+
+VALIDATE $? "Downloading rabbitmq script"
+
+dnf install rabbitmq-server -y  &>> $LOGFILE
+
+VALIDATE $? "Installing RabbitMQ server"
+
+systemctl enable rabbitmq-server &>> $LOGFILE
+
+VALIDATE $? "Enabling rabbitmq server"
+
+systemctl start rabbitmq-server  &>> $LOGFILE
+
+VALIDATE $? "Starting rabbitmq server"
+
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
+
+VALIDATE $? "creating user"
+
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> $LOGFILE
+
+VALIDATE $? "setting permission"
